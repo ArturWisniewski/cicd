9a95311cfb9e87fce82504a09f898ddc7120636f
Artur Wiśniewski <artur.inf@outlook.com>
2018-10-04 15:43:43 +0200

    test2

---
+++ ConsoleApplication/Calculator.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApplication
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ TestProject/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using ConsoleApplication;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(5, Calculator.Add(2,3));
+        }
+    }
+}
