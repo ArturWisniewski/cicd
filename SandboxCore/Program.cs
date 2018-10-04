3273eb2cf26692a0635cb1dcbc6c06acdd331d95
Artur Wiśniewski <artur.inf@outlook.com>
2018-10-04 13:12:09 +0200

    first commit

---
+++ SandboxCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandboxCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
